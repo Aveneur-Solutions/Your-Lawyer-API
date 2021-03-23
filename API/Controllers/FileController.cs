568817d3889bc4ff8f8cba2a502e25573e942f89
Ashik80 <ashikurrahman020995@gmail.com>
2021-03-23 11:00:17 +0600

    ocd fixed

--- API/Controllers/FileController.cs
+++ API/Controllers/FileController.cs
@@ -16,9 +16,6 @@ namespace API.Controllers
         [HttpPost("filex")]
         public async Task<ActionResult<QueryFileDTO>> UploadToUser([FromForm]UploadToUser.Command command )
         {
-
-
-            
             return await Mediator.Send(command);
         }
     }
