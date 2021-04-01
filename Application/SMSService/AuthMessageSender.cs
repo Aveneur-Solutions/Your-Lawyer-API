a22c679123bdd97ba6877c05a16385cf75d6c499
Zunaid Amin Enan <zunaid321@gmail.com>
2021-04-01 16:34:03 +0600

    New Twilio Account Added

--- Application/SMSService/AuthMessageSender.cs
+++ Application/SMSService/AuthMessageSender.cs
@@ -25,12 +25,12 @@ namespace Application.SMSService
             // Plug in your SMS service here to send a text message.
             // Your Account SID from twilio.com/console
             // configuration["SMSAccountIdentification"];
-            var accountSid = "AC37b2f92cb1260c2b17853791fbe9c48c";
+            var accountSid = "AC23f36b067597c578283aa6c24137feca";
             // Your Auth Token from twilio.com/console
             // var authToken = configuration["SMSAccountPassword"];
-            var authToken = "028699d10ce9aec10b855982b780d7c9";
+            var authToken = "6e1dbbf34352f979efdc19ca15699a0c";
             // var fromNumber = configuration["SMSAccountFrom"];
-            var fromNumber = "+12013748913";
+            var fromNumber = "+17702123329";
 
             TwilioClient.Init(accountSid, authToken);
 
